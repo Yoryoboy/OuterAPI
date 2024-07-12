240961a62c8a2012cefddaa30f8f61a5e2663f64
Yoryoboy <93jads@gmail.com>
2024-07-12 12:21:47 -0300

    Add welcome message to root

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 3000;
 
 app.use(json());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to OuterAPI",
+    version: "1.0.0",
+    description:
+      "API developed by Jorge Diaz. Intended to be consumed by Irazu Technology.For more info, reach out to: 93jads@gmail.com",
+  });
+});
+
 app.use("/webhook", webhookRoutes);
 
 app.listen(port, () => {
--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -2,7 +2,6 @@ import { handleMovingTaskToContractor } from "../services/webhookService.js";
 
 export function add_to_contractor_list(req, res) {
   const historyItems = req.body.history_items[0];
-
   const user = historyItems.user.username;
   const taskId = req.body.task_id;
   const { before, after } = historyItems;
