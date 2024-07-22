6905062b61375fe3c1217651d82dcd61fef4cee4
Yoryoboy <93jads@gmail.com>
2024-07-22 11:01:31 -0300

    Added error handling in validateField middleware to log incoming data on errors and return a 204 status code for unexpected data

--- src/middlewares/fieldValidator.js
+++ src/middlewares/fieldValidator.js
@@ -1,15 +1,22 @@
 export function validateField(req, res, next) {
-  const fieldUpdated = req.body.history_items.find(
-    (item) => item.custom_field && item.custom_field.name === "CONTRACTOR_KING"
-  );
+  try {
+    const fieldUpdated = req.body.history_items.find(
+      (item) =>
+        item.custom_field && item.custom_field.name === "CONTRACTOR_KING"
+    );
 
-  if (fieldUpdated) {
-    next();
-  } else {
-    const message =
-      "Campo no relevante actualizado, no se requiere procesamiento.";
-    res.status(200).json({
-      message: message,
+    if (fieldUpdated) {
+      next();
+    } else {
+      res.status(204).json({
+        message:
+          "Campo no relevante actualizado, no se requiere procesamiento.",
+      });
+    }
+  } catch (error) {
+    console.error("Error detected with incoming data", req.body, error);
+    res.status(204).json({
+      message: "Invalid request",
     });
   }
 }
